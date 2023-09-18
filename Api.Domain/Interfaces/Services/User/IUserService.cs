f8a4079dc197272c74ca6440d96e6bfc582a4222
immerfallen <marodemelo10@gmail.com>
2023-09-18 11:08:52 -0300

    Criada interaface para usuário na camada de domínio

---
+++ Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll();
+        Task<UserEntity> Post(UserEntity user);
+        Task<UserEntity> Put(UserEntity user);
+        Task<bool> Delete(Guid id);
+    }
+}
