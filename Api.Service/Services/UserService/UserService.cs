bd0101e23bcc92a3a7507211cea36e1fbce58c6b
immerfallen <marodemelo10@gmail.com>
2023-09-18 11:21:43 -0300

    implementada classe concreta user service

---
+++ Api.Service/Services/UserService/UserService.cs
@@ -0,0 +1,46 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Api.Domain.Interfaces.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Service.Services.UserService
+{
+    class UserService : IUserService
+    {
+
+        private IRepository<UserEntity> _repository;
+
+        public UserService(IRepository<UserEntity> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<UserEntity> Get(Guid id)
+        {
+            return await _repository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            return await _repository.SelectAsync();
+        }
+
+        public Task<UserEntity> Post(UserEntity user)
+        {
+            return _repository.InsertAsync(user);
+        }
+
+        public Task<UserEntity> Put(UserEntity user)
+        {
+            return _repository.UpdateAsync(user);
+        }
+    }
+}
