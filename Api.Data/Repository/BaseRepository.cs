3b06070db6cdd75eb519c799e149636bf4f4a423
immerfallen <marodemelo10@gmail.com>
2023-09-18 10:45:29 -0300

    Criado método de insert async na base repository

---
+++ Api.Data/Repository/BaseRepository.cs
@@ -0,0 +1,64 @@
+﻿using Api.Data.Context;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.Repository
+{
+    class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly MyContext _context;
+        private DbSet<T> _dataSet;
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+            _dataSet = _context.Set<T>();
+        }
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> InsertAsync(T item)
+        {
+            try
+            {
+                if(item.Id == Guid.Empty)
+                {
+                    item.Id = Guid.NewGuid();
+                }
+                item.CreatedAt = DateTime.UtcNow;
+                _dataSet.Add(item);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return  item;
+        }
+
+        public Task<T> SelectAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
