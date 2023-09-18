1f711c50351a6e92e91a953609586eeeb2e5b2b2
immerfallen <marodemelo10@gmail.com>
2023-09-18 13:11:59 -0300

    Criado método post no controlador de user

--- Api.application/Controllers/UserController.cs
+++ Api.application/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Api.Domain.Interfaces.Services.User;
+﻿using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -48,6 +49,32 @@ namespace Api.application.Controllers
             try
             {
                 return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException e)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var result = await _service.Post(user);
+                if(user != null)
+                {
+                    return Created(new Uri(Url.Link("GetById", new {id=result.Id,  })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
             }
             catch (ArgumentException e)
             {
