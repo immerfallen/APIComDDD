ca95fb452404dc52e68f5284526b4ccd01bb41e3
immerfallen <marodemelo10@gmail.com>
2023-09-18 14:26:26 -0300

    Feito serviço de login

---
+++ Api.Service/Services/LoginService/LoginService.cs
@@ -0,0 +1,32 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+using Api.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Service.Services.LoginService
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _repository;
+
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<object> FindByLogin(UserEntity user)
+        {           
+            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                return await _repository.FindByLogin(user.Email);                
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
