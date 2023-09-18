1082bc920a477b6a898abea215c3269423cea7f8
immerfallen <marodemelo10@gmail.com>
2023-09-18 11:48:51 -0300

    Criado controlador para user

---
+++ Api.application/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Api.application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
