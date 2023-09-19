6e8ee801864f80611ab5850be115bf9019894632
immerfallen <marodemelo10@gmail.com>
2023-09-19 10:49:27 -0300

    incluído seed inicial

--- Api.Data/Context/MyContext.cs
+++ Api.Data/Context/MyContext.cs
@@ -20,6 +20,17 @@ namespace Api.Data.Context
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+            modelBuilder.Entity<UserEntity>().HasData(
+
+                new UserEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Maro",
+                    Email = "maro@gmail.com",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now
+                });
+
         }
 
         
