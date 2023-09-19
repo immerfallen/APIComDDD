5882ce6e1c12cfa6f3c13b68b1ebef1c1b3cf822
immerfallen <marodemelo10@gmail.com>
2023-09-19 14:25:43 -0300

    Criado teste para crud usuário

---
+++ Api.Data.Test/UsuarioCrudCompleto.cs
@@ -0,0 +1,43 @@
+﻿using Api.Data.Context;
+using Api.Data.Implementations;
+using Api.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public class UsuarioCrudCompleto : BaseTeste, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+
+        public UsuarioCrudCompleto(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.ServiceProvider;            
+        }
+
+        [Fact(DisplayName = "CRUD de Usuário")]
+        [Trait("CRUD", "UserEntity")]
+        public async Task E_Possivel_Realizar_CRUD_Usuario()
+        {
+            using (var context = _serviceProvider.GetService<MyContext>())
+            {
+                UserImplementation _repository = new UserImplementation(context);
+                UserEntity _entity = new UserEntity()
+                {
+                    Email = "teste@gmail.com",
+                    Name= "Maro"
+                };
+                var _registroCriado = await _repository.InsertAsync(_entity);
+                Assert.NotNull(_registroCriado);
+                Assert.Equal("teste@gmail.com", _registroCriado.Email);
+                Assert.Equal("Maro", _registroCriado.Name);
+                Assert.False(_registroCriado.Id == Guid.Empty);
+            }
+        }
+    }
+}
