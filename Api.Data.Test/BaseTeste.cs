814e014cb4d820ec12455d0caae3824fbec0b440
immerfallen <marodemelo10@gmail.com>
2023-09-19 13:53:14 -0300

    Criada classe de base teste e conexão com o banco de testes

---
+++ Api.Data.Test/BaseTeste.cs
@@ -0,0 +1,44 @@
+﻿
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTeste
+    {
+        public BaseTeste()
+        {
+
+        }
+    }
+
+    public class DbTeste : IDisposable
+    {
+        private string dataBaseName = $"dbApiTeste_{Guid.NewGuid().ToString().Replace("-", "")}";
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTeste()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseSqlServer($"Server = (localdb)\\mssqllocaldb; Database ={dataBaseName}; Trusted_Connection = True; MultipleActiveResultSets = true"), ServiceLifetime.Transient
+                );
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
