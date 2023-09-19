1e363900cd5932f6ffbc43f974deb0bd26b83216
immerfallen <marodemelo10@gmail.com>
2023-09-19 08:57:08 -0300

    Criados DTOs para usuário

---
+++ Api.Domain/DTOs/User/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.DTOs.User
+{
+    public class UserDTO
+    {
+        [Required(ErrorMessage ="Nome é campo obrigatório")]
+        [StringLength(60, ErrorMessage = "Nome deve ter no máximo {1} caracteres")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email é campo obrigatório")]
+        [StringLength(100, ErrorMessage = "Nome deve ter no máximo {1} caracteres")]
+        [EmailAddress (ErrorMessage ="E-mail com formato inválido")]
+        public string Email { get; set; }
+    }
+}
---
+++ Api.Domain/DTOs/User/UserDTOUpdateResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.DTOs.User
+{
+    public class UserDTOUpdateResult
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
