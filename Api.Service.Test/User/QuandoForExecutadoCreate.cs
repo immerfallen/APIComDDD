e315281916d673e89ac85ebde5425d83afc6ac8b
immerfallen <marodemelo10@gmail.com>
2023-10-02 10:03:31 -0300

    Mocado post na camada de service

---
+++ Api.Service.Test/User/QuandoForExecutadoCreate.cs
@@ -0,0 +1,32 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+    public class QuandoForExecutadoCreate : UserTest
+    {
+        private IUserService _userService;
+        private Mock<IUserService> _userServiceMock;
+
+        [Fact(DisplayName = "É possivel executar o método Create.")]
+        public async Task E_Possivel_Executar_GetAll()
+        {
+            _userServiceMock = new Mock<IUserService>();
+            _userServiceMock.Setup(m => m.Post(userDTOCreate)).ReturnsAsync(userDTOCreateResult);
+            _userService = _userServiceMock.Object;
+
+            var result = await _userService.Post(userDTOCreate);
+            Assert.NotNull(result);
+            Assert.Equal(NomeUsuario, result.Name);
+            Assert.Equal(EmailUsuario, result.Email);
+            
+        }
+
+    }
+}
