0bbed792abcb3ed0fd37f8a66d3fe1e1a336c9cb
immerfallen <marodemelo10@gmail.com>
2023-10-02 09:56:47 -0300

    Mocado teste para getall da camada de service

---
+++ Api.Service.Test/User/QuandoForExecutadoGetAll.cs
@@ -0,0 +1,40 @@
+﻿using Api.Domain.DTOs.User;
+using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+    public class QuandoForExecutadoGetAll : UserTest
+    {
+        private IUserService _userService;
+        private Mock<IUserService> _userServiceMock;
+
+        [Fact(DisplayName = "É possivel executar o método GETALL.")]
+        public async Task E_Possivel_Executar_GetAll()
+        {
+            _userServiceMock = new Mock<IUserService>();
+            _userServiceMock.Setup(m => m.GetAll()).ReturnsAsync(listaUserDto);
+            _userService = _userServiceMock.Object;
+
+            var result = await _userService.GetAll();
+            Assert.NotNull(result);
+            Assert.True(result.Count() == 10);
+
+            var _listResult = new List<UserDTO>();
+
+            _userServiceMock = new Mock<IUserService>();
+            _userServiceMock.Setup(m => m.GetAll()).ReturnsAsync(_listResult.AsEnumerable);
+            _userService = _userServiceMock.Object;
+
+            var _resultEmpty = await _userService.GetAll();
+            Assert.Empty(_resultEmpty);
+            Assert.True(_resultEmpty.Count() == 0);
+        }
+    }
+}
