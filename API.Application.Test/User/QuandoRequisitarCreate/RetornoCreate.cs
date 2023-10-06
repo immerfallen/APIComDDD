11243c61be8abe072a8e8eebf75f06694edaeeec
immerfallen <marodemelo10@gmail.com>
2023-10-06 10:27:38 -0300

    Cobertura de testes do post na camada de application

---
+++ API.Application.Test/User/QuandoRequisitarCreate/RetornoCreate.cs
@@ -0,0 +1,55 @@
+﻿using Api.application.Controllers;
+using Api.Domain.DTOs.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace API.Application.Test.User.QuandoRequisitarCreate
+{
+    public class RetornoCreate
+    {
+        private UserController _controller;
+
+        [Fact(DisplayName ="É possível realizar Create")]
+        public async Task E_Possivel_Invocar_Controller_Create()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Post(It.IsAny<UserDTOCreate>())).ReturnsAsync(new UserDTOCreateResult
+            {
+                Id = Guid.NewGuid(),
+                Name= nome,
+                Email = email,
+                CreatedAt = DateTime.UtcNow
+            });
+
+            _controller = new UserController(serviceMock.Object);
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns("http://localhost:5000");
+            _controller.Url = url.Object;
+
+            var userDtoCreate = new UserDTOCreate
+            {
+                Name = nome,
+                Email = email
+            };
+
+            var result = await _controller.Post(userDtoCreate);
+            Assert.True(result is CreatedResult);
+
+            var resultValue = ((CreatedResult)result).Value as UserDTOCreateResult;
+            Assert.NotNull(resultValue);
+            Assert.Equal(userDtoCreate.Name, resultValue.Name);
+            Assert.Equal(userDtoCreate.Email, resultValue.Email);
+        }
+    }
+}
