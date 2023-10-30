7e0870a6589532ed5e483d4494ffb62f0879e705
immerfallen <marodemelo10@gmail.com>
2023-10-30 09:29:27 -0300

    Cobertura de testes para controlador get

---
+++ API.Application.Test/User/QuandoRequisitarGet/Retorno_BadRequest.cs
@@ -0,0 +1,44 @@
+﻿using Api.application.Controllers;
+using Api.Domain.DTOs.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace API.Application.Test.User.QuandoRequisitarGet
+{
+    public class Retorno_BadRequest
+    {
+        private UserController _controller;
+
+        [Fact(DisplayName = "É possível realizar Get")]
+        public async Task E_Possivel_Invocar_Controller_Get()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m
+                                                        .Get(It.IsAny<Guid>()))
+                                                        .ReturnsAsync(
+                    new UserDTO
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = nome,
+                        Email = email, 
+                        CreatedAt = DateTime.UtcNow
+                    });
+
+            _controller = new UserController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Email", "O e-mail é obrigatório" );
+
+            var result = await _controller.Get(Guid.NewGuid());
+            Assert.True(result is BadRequestObjectResult);
+        }
+    }
+}
